95403dba713de6f9123b37370b18da957595161e
jaturaphat2543 <6206021611095@fitm.kmutnb.ac.th>
2021-01-12 20:30:38 +0700

    create

---
+++ ex01.js
@@ -0,0 +1,6 @@
+function findperimeter(lenght,width){
+    return ((lenght+width)*2);
+}
+console.log(findperimeter(7,6));
+console.log(findperimeter(20,10));
+console.log(findperimeter(2,9));
---
+++ ex02.js
@@ -0,0 +1,7 @@
+function equation(value){
+    let result = eval(value);
+    return result;
+}
+console.log(equation("1+1"));
+console.log(equation("7*4-2"));
+console.log(equation("1+1+1+1+1"));
