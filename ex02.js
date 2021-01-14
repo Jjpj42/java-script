2120f160d4877c33e6d6307331f96db985858c7b
jaturaphat2543 <6206021611095@fitm.kmutnb.ac.th>
2021-01-14 13:56:23 +0700

    create

--- ex02.js
+++ ex02.js
@@ -1,6 +1,33 @@
 function equation(value){
-    let result = eval(value);
-    return result;
+
+   var strva=value;
+   var numstr='';
+   var oparetor='';
+   var total;
+   
+for(i=0;i<value.length;i++){
+    if(strva[i]=='+') oparetor += '+';
+    else if (strva[i]=='-') oparetor +='-';
+    else if (strva[i]=='*') oparetor +='*';
+    else numstr+=strva[i];
+}
+
+    total=parseInt(numstr[0]);
+    for(i=0;i<numstr.length;i++){
+        if(oparetor[i-1]=='+') total+=parseInt(numstr[i]);
+        else if (oparetor[i-1]=='-') total-=parseInt(numstr[i]);
+        else if (oparetor[i-1]=='*') total*=parseInt(numstr[i]);
+    }
+    
+   
+
+    
+
+    
+
+
+    return total;
+
 }
 console.log(equation("1+1"));
 console.log(equation("7*4-2"));
