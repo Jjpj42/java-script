019b73c57a052c8b57b78e569137b25812afdc44
jaturaphat2543 <6206021611095@fitm.kmutnb.ac.th>
2021-02-18 08:20:30 +0700

    update

---
+++ morePage.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!-- ชื่อไฟล์ : Exam06-11.php -->
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title></title></head>
+</head>
+<body>
+<form method="post" action="morePageCal.php">
+<table border="1" align="center" width="500">
+
+
+<td>First Name : </td>
+<td><input type="text" name="first" size="15" value=""/></td>
+</tr>
+</tr>
+<td>Last Name  : </td>
+<td><input type="text" name="last" size="15" value=""/></td>
+</tr>
+<tr>
+<td colspan="2" align="center">
+<input type="submit" value=" Send " />
+<input type="reset" value=" Clear " />
+</td>
+</tr>
+</table>
+</form>
+</body>
+</html>
